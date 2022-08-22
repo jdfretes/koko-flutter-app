995fb0f21a05509c0cd831ede9bdede5aac60bb5
donpol <fretesjuan4@gmail.com>
2022-08-21 21:43:49 -0400

    tabbbar

--- lib/Views/HomeView.dart
+++ lib/Views/HomeView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/scheduler/ticker.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:koko/CustomWidgets/CarouselText.dart';
 import 'package:koko/CustomWidgets/TitleText.dart';
@@ -15,7 +16,7 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
   ImageUtils imageUtils = ImageUtils();
   final List<String> categoriesList = [
     "Todos",
@@ -25,133 +26,172 @@ class _HomeViewState extends State<HomeView> {
     "Peces"
   ];
 
+  late TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: const CustomAppBar(),
-        body: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-              child: const CustomTextField(),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const TitleText(
-                    title: DefaultStrings.Categories,
+      backgroundColor: Colors.brown[50],
+      appBar: const CustomAppBar(),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: const CustomTextField(),
+          ),
+          Container(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const TitleText(
+                  title: DefaultStrings.Categories,
+                ),
+                CarouselText(categoriesList: categoriesList),
+                Container(
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TitleText(title: DefaultStrings.NearYou),
+                      Text(
+                        DefaultStrings.SeeAll,
+                        style: TextStyle(
+                            fontFamily: FontsFamily.OpenSansSemiBold,
+                            color: Colors.brown.shade600),
+                      )
+                    ],
                   ),
-                  CarouselText(categoriesList: categoriesList),
-                  Container(
-                    margin: const EdgeInsets.only(top: 32),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const TitleText(title: DefaultStrings.NearYou),
-                        Text(
-                          DefaultStrings.SeeAll,
-                          style: TextStyle(
-                              fontFamily: FontsFamily.OpenSansSemiBold,
-                              color: Colors.brown.shade600),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
-            GFItemsCarousel(
-              itemHeight: 350,
-              rowCount: 2,
-              children: imageUtils.images.map(
-                (url) {
-                  return Container(
-                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-                    child: ClipRRect(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(5.0)),
-                        child: Container(
-                          padding: const EdgeInsets.all(16.0),
-                          color: Colors.brown.shade100,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              ClipRRect(
-                                borderRadius: const BorderRadius.all(
-                                    Radius.circular(5.0)),
-                                child: Container(
-                                  child: Image.network(
-                                    url,
-                                    height: 200,
-                                    fit: BoxFit.cover,
-                                  ),
+          ),
+          GFItemsCarousel(
+            itemHeight: 350,
+            rowCount: 2,
+            children: imageUtils.images.map(
+              (url) {
+                return Container(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                  child: ClipRRect(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0)),
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        color: Colors.brown.shade100,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(5.0)),
+                              child: Container(
+                                child: Image.network(
+                                  url,
+                                  height: 200,
+                                  fit: BoxFit.cover,
                                 ),
                               ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 16),
-                                child: const TitleText(title: "Nombre"),
-                              ),
-                              Container(
-                                margin: const EdgeInsets.only(top: 16),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Macho",
-                                      style: TextStyle(
-                                          fontFamily:
-                                              FontsFamily.OpenSansSemiBold,
-                                          color: Colors.brown.shade400),
-                                    ),
-                                    Text(
-                                      "1a 3m",
-                                      style: TextStyle(
-                                          fontFamily:
-                                              FontsFamily.OpenSansSemiBold,
-                                          color: Colors.brown.shade400),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Row(
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 16),
+                              child: const TitleText(title: "Nombre"),
+                            ),
+                            Container(
+                              margin: const EdgeInsets.only(top: 16),
+                              child: Row(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceBetween,
                                 children: [
-                                  GFIconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.favorite_border,
-                                      color: Colors.brown.shade700,
-                                    ),
-                                    type: GFButtonType.solid,
-                                    color: Colors.white,
-                                    size: GFSize.SMALL,
+                                  Text(
+                                    "Macho",
+                                    style: TextStyle(
+                                        fontFamily:
+                                            FontsFamily.OpenSansSemiBold,
+                                        color: Colors.brown.shade400),
                                   ),
-                                  GFButton(
-                                    onPressed: () {},
-                                    color: Colors.brown,
-                                    child: const Text("Adoptar",
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 18,
-                                            fontFamily:
-                                                FontsFamily.OpenSansSemiBold,
-                                            decoration: TextDecoration.none)),
+                                  Text(
+                                    "1a 3m",
+                                    style: TextStyle(
+                                        fontFamily:
+                                            FontsFamily.OpenSansSemiBold,
+                                        color: Colors.brown.shade400),
                                   )
                                 ],
-                              )
-                            ],
-                          ),
-                        )),
-                  );
-                },
-              ).toList(),
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                GFIconButton(
+                                  onPressed: () {},
+                                  icon: Icon(
+                                    Icons.favorite_border,
+                                    color: Colors.brown.shade700,
+                                  ),
+                                  type: GFButtonType.solid,
+                                  color: Colors.white,
+                                  size: GFSize.SMALL,
+                                ),
+                                GFButton(
+                                  onPressed: () {},
+                                  color: Colors.brown,
+                                  child: const Text("Adoptar",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                          fontFamily:
+                                              FontsFamily.OpenSansSemiBold,
+                                          decoration: TextDecoration.none)),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                      )),
+                );
+              },
+            ).toList(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: GFTabBar(
+        tabBarColor: Colors.black,
+        length: 3,
+        controller: tabController,
+        tabs: const [
+          Tab(
+            icon: Icon(Icons.home),
+            child: Text(
+              "Inicio",
             ),
-          ],
-        ));
+          ),
+          Tab(
+            icon: Icon(Icons.favorite_outline),
+            child: Text(
+              "Favoritos",
+            ),
+          ),
+          Tab(
+            icon: Icon(Icons.person),
+            child: Text(
+              "Perfil",
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
