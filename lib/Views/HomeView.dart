f1095fcad4bd44855c88c41e0098388bb1509de2
donpol <fretesjuan4@gmail.com>
2022-08-21 21:31:09 -0400

    adopt section

--- lib/Views/HomeView.dart
+++ lib/Views/HomeView.dart
@@ -6,6 +6,7 @@ import 'package:koko/CustomWidgets/CustomAppBar.dart';
 import 'package:koko/CustomWidgets/CustomTextField.dart';
 import 'package:koko/Utils/DefaultStrings.dart';
 import 'package:koko/Utils/FontsFamily.dart';
+import 'package:koko/Utils/ImageUtils.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -15,6 +16,7 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  ImageUtils imageUtils = ImageUtils();
   final List<String> categoriesList = [
     "Todos",
     "Perros",
@@ -39,21 +41,116 @@ class _HomeViewState extends State<HomeView> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const TitleText(title: DefaultStrings.Categories,),
+                  const TitleText(
+                    title: DefaultStrings.Categories,
+                  ),
                   CarouselText(categoriesList: categoriesList),
                   Container(
                     margin: const EdgeInsets.only(top: 32),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                         const TitleText(title: DefaultStrings.NearYou),
-                         Text(DefaultStrings.SeeAll, style: TextStyle(fontFamily: FontsFamily.OpenSansSemiBold, color: Colors.brown.shade600),)
+                        const TitleText(title: DefaultStrings.NearYou),
+                        Text(
+                          DefaultStrings.SeeAll,
+                          style: TextStyle(
+                              fontFamily: FontsFamily.OpenSansSemiBold,
+                              color: Colors.brown.shade600),
+                        )
                       ],
                     ),
                   )
                 ],
               ),
-            )
+            ),
+            GFItemsCarousel(
+              itemHeight: 350,
+              rowCount: 2,
+              children: imageUtils.images.map(
+                (url) {
+                  return Container(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                    child: ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(5.0)),
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          color: Colors.brown.shade100,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(5.0)),
+                                child: Container(
+                                  child: Image.network(
+                                    url,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 16),
+                                child: const TitleText(title: "Nombre"),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 16),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Macho",
+                                      style: TextStyle(
+                                          fontFamily:
+                                              FontsFamily.OpenSansSemiBold,
+                                          color: Colors.brown.shade400),
+                                    ),
+                                    Text(
+                                      "1a 3m",
+                                      style: TextStyle(
+                                          fontFamily:
+                                              FontsFamily.OpenSansSemiBold,
+                                          color: Colors.brown.shade400),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  GFIconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.favorite_border,
+                                      color: Colors.brown.shade700,
+                                    ),
+                                    type: GFButtonType.solid,
+                                    color: Colors.white,
+                                    size: GFSize.SMALL,
+                                  ),
+                                  GFButton(
+                                    onPressed: () {},
+                                    color: Colors.brown,
+                                    child: const Text("Adoptar",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontFamily:
+                                                FontsFamily.OpenSansSemiBold,
+                                            decoration: TextDecoration.none)),
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        )),
+                  );
+                },
+              ).toList(),
+            ),
           ],
         ));
   }
