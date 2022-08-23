b80e0a5c383ccc5e88c8c6ed78a2b98ad39269e4
donpol <fretesjuan4@gmail.com>
2022-08-22 21:01:16 -0400

    fix

---
+++ lib/CustomWidgets/custom_card_item.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:koko/CustomWidgets/title_text.dart';
+import 'package:koko/Utils/fonts_family.dart';
+
+class CustomCardItem extends StatelessWidget {
+  const CustomCardItem({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+      child: ClipRRect(
+          borderRadius:
+              const BorderRadius.all(Radius.circular(5.0)),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            color: Colors.brown.shade100,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius:
+                      const BorderRadius.all(Radius.circular(5.0)),
+                  child: SizedBox(
+                    height: 200,
+                    width: 200,
+                    child: Image.network(
+                      url,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  child: const TitleText(title: "Nombre"),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Macho",
+                        style: TextStyle(
+                            fontFamily:
+                                FontsFamily.openSansSemiBold,
+                            color: Colors.brown.shade400),
+                      ),
+                      Text(
+                        "1a 3m",
+                        style: TextStyle(
+                            fontFamily:
+                                FontsFamily.openSansSemiBold,
+                            color: Colors.brown.shade400),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GFIconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite_border,
+                        color: Colors.brown.shade700,
+                      ),
+                      type: GFButtonType.solid,
+                      color: Colors.white,
+                      size: GFSize.SMALL,
+                    ),
+                    GFButton(
+                      onPressed: () {},
+                      color: Colors.brown,
+                      child: const Text("Adoptar",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontFamily:
+                                  FontsFamily.openSansSemiBold,
+                              decoration: TextDecoration.none)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+  }
+}
--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getwidget/getwidget.dart';
 import 'package:koko/CustomWidgets/carousel_text.dart';
+import 'package:koko/CustomWidgets/custom_card_item.dart';
 import 'package:koko/CustomWidgets/title_text.dart';
 import 'package:koko/CustomWidgets/custom_app_bar.dart';
 import 'package:koko/CustomWidgets/custom_text_field.dart';
@@ -82,85 +83,7 @@ class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
             rowCount: 2,
             children: imageUtils.images.map(
               (url) {
-                return Container(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
-                  child: ClipRRect(
-                      borderRadius:
-                          const BorderRadius.all(Radius.circular(5.0)),
-                      child: Container(
-                        padding: const EdgeInsets.all(16.0),
-                        color: Colors.brown.shade100,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(5.0)),
-                              child: Container(
-                                child: Image.network(
-                                  url,
-                                  height: 200,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 16),
-                              child: const TitleText(title: "Nombre"),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 16),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Macho",
-                                    style: TextStyle(
-                                        fontFamily:
-                                            FontsFamily.openSansSemiBold,
-                                        color: Colors.brown.shade400),
-                                  ),
-                                  Text(
-                                    "1a 3m",
-                                    style: TextStyle(
-                                        fontFamily:
-                                            FontsFamily.openSansSemiBold,
-                                        color: Colors.brown.shade400),
-                                  )
-                                ],
-                              ),
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                GFIconButton(
-                                  onPressed: () {},
-                                  icon: Icon(
-                                    Icons.favorite_border,
-                                    color: Colors.brown.shade700,
-                                  ),
-                                  type: GFButtonType.solid,
-                                  color: Colors.white,
-                                  size: GFSize.SMALL,
-                                ),
-                                GFButton(
-                                  onPressed: () {},
-                                  color: Colors.brown,
-                                  child: const Text("Adoptar",
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18,
-                                          fontFamily:
-                                              FontsFamily.openSansSemiBold,
-                                          decoration: TextDecoration.none)),
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      )),
-                );
+                return CustomCardItem(url: url);
               },
             ).toList(),
           ),
